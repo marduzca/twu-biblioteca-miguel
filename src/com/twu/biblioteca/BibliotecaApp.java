563f639559abd35d194016d5a6281d979f431418
miguel.arduz <miguel.arduz@thoughtworks.com>
2019-04-10 18:01:05 +0200

    Added display of book list with title, author and year

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,6 +4,18 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         System.out.println(showWelcomeMessage());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        BookList bList = new BookList();
+        System.out.println(bList.showListOfBooks());
+
+        System.out.println(bList.showListOfBooksWithExtraInfo());
+
     }
 
     public static String showWelcomeMessage() {
